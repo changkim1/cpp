cb165dbfa9fe748347e842145d99c93d8f894908
changkim <zzankor@naver.com>
2022-10-06 17:42:57 +0900

    221006

---
+++ bjcp/1240.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+queue<pair<short, short>> q;
+short map[1001][1001];
+
+short n, m;
+
+int main(void)
+{
+	short a, b, c;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		
+	}
+}
