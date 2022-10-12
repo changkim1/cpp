13e00c7a5e0180d0aaac06d154893b9980c6d5ca
changkim <zzankor@naver.com>
2022-10-12 14:52:52 +0900

    221012

---
+++ bjcp/11047.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, k;
+int price[11];
+
+int compare(int a, int b)
+{
+	return (a > b);
+}
+
+int main(void)
+{
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> price[i];
+	}
+	sort(price, price + n, compare);
+	int tmp, i, cnt;
+	cnt = 0;
+	tmp = k;
+	while (tmp != 0)
+	{
+		while (price[i] != 0 && price[i] > tmp)
+			i++;
+		tmp -= price[i];
+		cnt++;
+	}
+	cout << cnt << endl;
+}
