91d72995d7ddf6f9912b08839b6b2e6146c4e498
changkim <zzankor@naver.com>
2022-10-13 19:55:46 +0900

    221013

--- bjcp/2579.cpp
+++ bjcp/2579.cpp
@@ -14,13 +14,14 @@ int main(void)
 	{
 		cin >> arr[i];
 	}
-	dp[0] = 0;
-	arr[0] = 0;
 	dp[1] = arr[1];
 	dp[2] = arr[1] + arr[2];
 	for (int i = 3; i <= n; i++)
 	{
-		dp[i] = MAX(dp[i-3] + arr[i-1], dp[i-2]) + arr[i];
+		dp[i] = MAX((dp[i - 3] + arr[i-1]), dp[i-2]) + arr[i];
+		printf("a = %d b = %d MAX = %d arr[%d] = %d\n", dp[i-3] + arr[i-1], dp[i-2], MAX(dp[i-3] + arr[i-1], dp[i-2]), i, arr[i]);
+		printf("dp[%d] = %d\n", i, dp[i]);
 	}
-	printf("%d\n", dp[n]);
+	cout << endl;
+	
 }
