16854a535152d4ec7731932e4f90398f593839d9
changkim <zzankor@naver.com>
2022-10-14 10:35:56 +0900

    221014

---
+++ bjcp/15649.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <map>
+
+using namespace std;
+
+priority_queue<string> pq;
+map<string, int> _map;
+map<char, int> tmp;
+
+int n, m;
+
+int	is_okay(int idx)
+{
+	if ()
+	{
+
+	}
+	tmp.clear();
+}
+
+int main(void)
+{
+	
+}
